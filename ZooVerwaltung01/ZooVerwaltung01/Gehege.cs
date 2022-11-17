1e7d4b44ad7aff0fb63c11bb76e592d799ef5418
RamyBit <118202722+RamyBit@users.noreply.github.com>
2022-11-17 20:04:14 +0100

    Gehege added

--- ZooVerwaltung01/ZooVerwaltung01/Gehege.cs
+++ ZooVerwaltung01/ZooVerwaltung01/Gehege.cs
@@ -8,5 +8,11 @@ namespace ZooVerwaltung01
 {
     class Gehege
     {
+        private List<Tier> tiere = new List<Tier>();
+
+        public void AddTier(Tier tier)
+        {
+            tiere.Add(tier);
+        }
     }
 }
