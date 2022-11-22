5e7e30c2161476894551a165fa97421b733228d6
RamyBit <118202722+RamyBit@users.noreply.github.com>
2022-11-22 17:41:38 +0100

    Hause Update

--- ITOverride/ITOverride/Program.cs
+++ ITOverride/ITOverride/Program.cs
@@ -12,6 +12,16 @@ namespace ITOverride
         {
             IEnjoyAble[] eineContainer = new IEnjoyAble[3];
             Fachinformatiker fachinformatiker = new Fachinformatiker();
+            Fiea fiea = new Fiea();
+            Fisi fisi = new Fisi();
+            eineContainer[0] = fachinformatiker;
+            eineContainer[1] = fiea;
+            eineContainer[2] = fisi;
+            foreach (Fachinformatiker person in eineContainer)
+            {
+                person.lernen();
+            }
+            Console.ReadLine();
         }
     }
 }
