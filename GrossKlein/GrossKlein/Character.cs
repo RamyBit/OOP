ef551d5a1d711dcbdfb20567b5ef964a64b6061d
RamyBit <118202722+RamyBit@users.noreply.github.com>
2022-12-06 15:52:08 +0100

    UpperCase/LowerCase

---
+++ GrossKlein/GrossKlein/Character.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrossKlein
+{
+    class Character
+    {
+        static public void UpperCase(string eingabe)
+        {
+            string ausgabe;
+            ausgabe = eingabe.ToUpper();
+            Console.WriteLine(ausgabe);
+        }
+        static public void LowerCase(string eingabe)
+        {
+            string ausgabe;
+            ausgabe = eingabe.ToLower();
+            Console.WriteLine(ausgabe);
+        }
+        static public void UpperLower(string eingabe)
+        {
+            string ausgabe = "";
+            foreach (var c in eingabe)
+            {
+                if (char.IsUpper(c))
+                    ausgabe += char.ToLower(c);
+                else
+                    ausgabe += char.ToUpper(c);
+            }
+            Console.WriteLine(ausgabe);
+        }
+    }
+}
---
+++ GrossKlein/GrossKlein/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrossKlein
+{
+    delegate void CharacterDelegate(string eingabe);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testText = "Diese Text wird getestet\"Aloha aus HomeOffice\"";
+            CharacterDelegate cDelegate;
+            cDelegate = Character.LowerCase;
+            cDelegate += Character.UpperCase;
+            cDelegate += Character.UpperLower;
+            cDelegate(testText);
+            
+            Console.ReadLine();
+            
+        }
+    }
+}
