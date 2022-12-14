267cf5dc13002b2772deb258024654b20aabecec
RamyBit <118202722+RamyBit@users.noreply.github.com>
2022-12-14 15:03:07 +0100

    ArbeitenMitDatei

---
+++ ArbeitenMitDateien/ArbeitenMitDateien/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ArbeitenMitDateien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileStream fs = File.Open(@"datei.txt", FileMode.Open);
+            //StreamReader sr = new StreamReader(fs);
+            //Console.WriteLine(sr.ReadToEnd());
+            //fs.Close();
+            //Console.ReadLine();
+
+            StreamReader sr = new StreamReader("datei.txt");
+
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
